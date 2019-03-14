85a4ec7478a009c0a69da32cb89815e6f0f547b0
rafayel23 <48155011+rafayel23@users.noreply.github.com>
2019-03-15 00:54:56 +0400

    created 'tab' and 'tabs' components

--- Rafo/tabs/src/app/app.module.ts
+++ Rafo/tabs/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule
