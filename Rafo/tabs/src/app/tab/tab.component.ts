51d2becfb70efb3e4a139f7ca36738c1dda549fc
rafayel23 <48155011+rafayel23@users.noreply.github.com>
2019-03-15 21:28:13 +0400

    replaced 3 Inputs with single Object

--- Rafo/tabs/src/app/tab/tab.component.ts
+++ Rafo/tabs/src/app/tab/tab.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TabComponent implements OnInit {
 
-  @Input() selectedTopicTitle: string;
-  @Input() targetIndex: number;
-  @Input() currentColor: string;
+  @Input() info: Object;
 
   descriptions: string[] = [
     'Home is where the heart is..',
